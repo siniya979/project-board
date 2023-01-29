d11868ca71987e34365591950b980d45b165492f
Shinny <siniya979@gmail.com>
2023-01-30 03:24:25 +0900

    # 11 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매칭되도록 설계. 
    생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게 설정.

--- spring-project-board/src/main/java/com/shinny/projectboard/domain/Article.java
+++ spring-project-board/src/main/java/com/shinny/projectboard/domain/Article.java
@@ -1,15 +1,82 @@
 package com.shinny.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAT"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; // 제목
-    private String content; // 내용
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일자
-    private  String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private  String modifiedBy; // 수정
+    // Setter 를 필드에 따로 세팅하는 이유는 특정 데이터만 client 가 값을 변경하도록 하기 위함.
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 내용
+
+    // 옵션 설정이 없을 경우 @Column 붙이지 않아도 자동으로 스프링이 붙여준다.
+    @Setter private String hashtag; // 해시태그
+
+    @ToString.Exclude
+    @OrderBy("id") // id 기준으로 정렬
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    // 아래의 데이터는 jpa 가 자동 생성 - jpa auditing
+    // @CreateBy 사용자에 대한 정보를 전달하기 위해 JpaConfig 에서 설정.
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일자
+    @CreatedBy @Column(nullable = false, length = 100) private  String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    // 생성자를 private 으로 막고 팩토리 메소드를 이용해 제공.
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // 동일성 동등성 검사를 위한 equals/hashCode 메서드 오버라이딩
+    // 클래스에 @EqualsANndHashCode 를 사용하면 필드값 전부를 검사하는데,
+    // 사실상 유니크키인 id 값만 비교해도 되기 때문에 필드에다 따로 설정.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id!=null && id.equals(article.id);
+        // 영속화를 하지 않았을 때(아직 db에 데이터를 insert 하지 않았을 때) entity 는 id를 부여 밭지 않았다.
+        // 따라서 id 가 null 이다. id!=null 를 추가해서 null 체크를 해야한다.
+        // 양속화 되지 않은 엔티티는 모두 동등성 검사를 탈락한다.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
