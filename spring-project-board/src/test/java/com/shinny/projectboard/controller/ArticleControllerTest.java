d8638fdea485cb40c2eb8366290fef2c41f67238
Shinny <siniya979@gmail.com>
2024-01-17 10:10:56 +0900

    #17 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하도록 오픈.
    뷰 기본 골격 작성
    테스트에는 media type 검사에서 부가적인 옵션이 포함될 수 있기 때문에 적절히 테스트 메소드 수정

--- spring-project-board/src/test/java/com/shinny/projectboard/controller/ArticleControllerTest.java
+++ spring-project-board/src/test/java/com/shinny/projectboard/controller/ArticleControllerTest.java
@@ -24,7 +24,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled // 테스트 제외 ( 개발중임으로 )
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -33,7 +32,7 @@ class ArticleControllerTest {
         //When&Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
