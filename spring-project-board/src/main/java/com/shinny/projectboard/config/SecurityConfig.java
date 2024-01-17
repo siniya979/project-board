28276f14ab6889fd21fea115ec6ee019effa9035
Shinny <siniya979@gmail.com>
2024-01-17 17:38:29 +0900

    #17 - 시큐리티 설정을 모든 요청에 인증 허가하도록 수정
    현재는 개발 단계이며, 인증 기능 구현 단계에 이르지 않았으므로 모든 인증 설정을 오픈한다. 또한 폼 로그인 활성화하여 로그인 뷰가 보이도록 함.
    
    스프링부트 2.7 버전 이상의 시큐리티 설정 방법을 활용

---
+++ spring-project-board/src/main/java/com/shinny/projectboard/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.shinny.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// @EnableWebSecurity 해당 어노테이션은 SpringBoot 에서 사용할 때 auto-config 에 등록되어 있다.
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(auth->auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
+}
